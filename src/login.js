b7f6764a8977b33d35b31c4ababa3c51e1bc3077
Bharat525 <see.rohit525@gmial.com>
2021-09-30 19:34:52 +0530

    changes

--- src/login.js
+++ src/login.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { GoogleLogin, GoogleLogout } from "react-google-login";
 
 const clientId =
-  "811339078289-1218asdmvo32tnqb2e48ghqcidtuj60s.apps.googleusercontent.com";
+  "731296744618-tknvl9hdra1e7dpfq0qmkpanvfcodgdi.apps.googleusercontent.com";
 
 function Login() {
   const [showloginButton, setShowloginButton] = useState(true);
